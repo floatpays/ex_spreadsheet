8bd8b94264af09b2a0d2f0fed8faa1b318ed937e
Wilhelm Kirschbaum <wkirschbaum@gmail.com>
2025-04-03 14:59:25 +0200

    Fix nif targets and versions

--- lib/spreadsheet/calamine.ex
+++ lib/spreadsheet/calamine.ex
@@ -6,11 +6,29 @@ defmodule Spreadsheet.Calamine do
   version = config[:version]
   github_url = config[:package][:links]["GitHub"]
 
+  targets = ~w(
+    aarch64-apple-darwin
+    aarch64-unknown-linux-gnu
+    aarch64-unknown-linux-musl
+    riscv64gc-unknown-linux-gnu
+    x86_64-apple-darwin
+    x86_64-pc-windows-gnu
+    x86_64-pc-windows-msvc
+    x86_64-unknown-linux-gnu
+    x86_64-unknown-linux-musl
+  )
+
+  nif_versions = ~w(
+    2.16
+  )
+
   use RustlerPrecompiled,
     otp_app: :spreadsheet,
     crate: "spreadsheet",
     base_url: "#{github_url}/releases/download/v#{version}",
-    version: version
+    version: version,
+    targets: targets,
+    nif_versions: nif_versions
 
   def sheet_names_from_binary(_content),
     do: :erlang.nif_error(:nif_not_loaded)
