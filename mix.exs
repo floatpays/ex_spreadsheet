52a416461981ff2bc0bdf6536b3fc01f01cfb1b1
Wilhelm Kirschbaum <wkirschbaum@gmail.com>
2025-08-06 16:18:53 +0200

    release: bump version

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule Spreadsheet.MixProject do
   use Mix.Project
 
   @github_url "https://github.com/floatpays/ex_spreadsheet"
-  @version "0.2.2"
+  @version "0.2.3"
 
   def project do
     [
