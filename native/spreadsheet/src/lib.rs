d1e971e0bbca906c7502e85f52ff298b426108dc
Wilhelm Kirschbaum <wkirschbaum@gmail.com>
2025-08-06 16:35:36 +0200

    fix: revert previous change as it fixes nothing

--- native/spreadsheet/src/lib.rs
+++ native/spreadsheet/src/lib.rs
@@ -1,12 +1,10 @@
-use calamine::{open_workbook_auto, open_workbook_auto_from_rs, Data, Reader, SheetVisible};
+use calamine::{
+    open_workbook_auto, open_workbook_auto_from_rs, Data, Reader, SheetVisible,
+};
 use rustler::{Binary, NifTaggedEnum};
 use std::io::Cursor;
 
-fn filter_sheet_names_by_visibility(
-    sheet_names: &[String],
-    sheets_metadata: &[calamine::Sheet],
-    show_hidden: bool,
-) -> Vec<String> {
+fn filter_sheet_names_by_visibility(sheet_names: &[String], sheets_metadata: &[calamine::Sheet], show_hidden: bool) -> Vec<String> {
     if show_hidden {
         sheet_names.to_owned()
     } else {
@@ -27,13 +25,9 @@ fn sheet_names_from_binary(content: Binary, show_hidden: bool) -> Result<Vec<Str
         Ok(workbook) => {
             let sheet_names = workbook.sheet_names();
             let sheets_metadata = workbook.sheets_metadata();
-            Ok(filter_sheet_names_by_visibility(
-                &sheet_names,
-                sheets_metadata,
-                show_hidden,
-            ))
+            Ok(filter_sheet_names_by_visibility(&sheet_names, sheets_metadata, show_hidden))
         }
-        Err(e) => Err(e.to_string()),
+        Err(e) => Err(e.to_string())
     }
 }
 
@@ -43,60 +37,50 @@ fn sheet_names_from_path(path: &str, show_hidden: bool) -> Result<Vec<String>, S
         Ok(workbook) => {
             let sheet_names = workbook.sheet_names();
             let sheets_metadata = workbook.sheets_metadata();
-            Ok(filter_sheet_names_by_visibility(
-                &sheet_names,
-                sheets_metadata,
-                show_hidden,
-            ))
+            Ok(filter_sheet_names_by_visibility(&sheet_names, sheets_metadata, show_hidden))
         }
-        Err(e) => Err(e.to_string()),
+        Err(e) => Err(e.to_string())
     }
 }
 
 #[rustler::nif]
-fn parse_from_path(path: &str, sheet_name: &str) -> Result<Vec<Vec<ColumnData>>, String> {
+fn parse_from_path(
+    path: &str,
+    sheet_name: &str,
+) -> Result<Vec<Vec<ColumnData>>, String> {
     let result = open_workbook_auto(path);
 
     match result {
-        Ok(mut workbook) => {
-            match workbook.worksheet_range(sheet_name) {
-                Ok(range) => Ok(range
+        Ok(mut workbook) => workbook
+            .worksheet_range(sheet_name)
+            .map_err(|e| e.to_string())
+            .map(|range| {
+                range
                     .rows()
                     .map(|row| row.iter().map(extract_column).collect::<Vec<_>>())
-                    .collect()),
-                Err(_e) => {
-                    // We don't want to return an error for invalid columns,
-                    // so let us just ignore them rather than failing for the
-                    // entire file. In the future we might want to show this
-                    // as a warning.
-                    Ok(Vec::new())
-                }
-            }
-        }
+                    .collect()
+            }),
         Err(e) => Err(e.to_string()),
     }
 }
 
 #[rustler::nif]
-fn parse_from_binary(content: Binary, sheet_name: &str) -> Result<Vec<Vec<ColumnData>>, String> {
+fn parse_from_binary(
+    content: Binary,
+    sheet_name: &str,
+) -> Result<Vec<Vec<ColumnData>>, String> {
     let result = open_workbook_auto_from_rs(Cursor::new(content.as_slice()));
 
     match result {
-        Ok(mut workbook) => {
-            match workbook.worksheet_range(sheet_name) {
-                Ok(range) => Ok(range
+        Ok(mut workbook) => workbook
+            .worksheet_range(sheet_name)
+            .map_err(|e| e.to_string())
+            .map(|range| {
+                range
                     .rows()
                     .map(|row| row.iter().map(extract_column).collect::<Vec<_>>())
-                    .collect()),
-                Err(_e) => {
-                    // We don't want to return an error for invalid columns,
-                    // so let us just ignore them rather than failing for the
-                    // entire file. In the future we might want to show this
-                    // as a warning.
-                    Ok(Vec::new())
-                }
-            }
-        }
+                    .collect()
+            }),
         Err(e) => Err(e.to_string()),
     }
 }
